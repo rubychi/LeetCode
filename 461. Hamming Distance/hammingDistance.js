aa173215e5cbc80e348cf7ac5d51adbfb8994338
RubyChi <nothelse@hotmail.com>
2017-08-30 22:30:54 +0800

    Publish gitbook

--- 461. Hamming Distance/hammingDistance.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {number} x
- * @param {number} y
- * @return {number}
- */
-var hammingDistance = function(x, y) {
-    let result = x ^ y;
-	return result.toString(2).replace(/0/g, "").length;
-};
